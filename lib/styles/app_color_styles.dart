992f86530e52859aa972a8c1d0e020f1ece720ee
Ko-Eunseo <koyk0408@naver.com>
2023-12-03 18:44:35 +0900

    design: 컬러 세팅 #1

---
+++ lib/styles/app_color_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const redColor = Color(0xffD14836);
+  static const yellowColor = Color(0xffECD06F);
+  static const greenColor = Color(0xff60AC9F);
+  static const mintColor = Color(0xffB1D2CB);
+  static const blueColor = Color(0xff0C0A51);
+  static const background = Color(0xffF0EDE6);
+  static const greyColor = Color(0xffDDDDDD);
+}
