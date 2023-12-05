979b84c9abc4b4edf84b88352424bc7908f14577
Ko-Eunseo <koyk0408@naver.com>
2023-12-06 01:21:23 +0900

    refactor: 색상 없어도 기본적으로 커스텀blue 컬러로 세팅되도록 수정

--- lib/view/widgets/headline.dart
+++ lib/view/widgets/headline.dart
@@ -1,21 +1,24 @@
+import 'package:christmas_postcard/styles/app_color_styles.dart';
 import 'package:christmas_postcard/styles/app_font_styles.dart';
 import 'package:flutter/material.dart';
 
 class Headline extends StatelessWidget {
   final String text;
-  final Color textColor;
+  final Color? textColor;
 
   const Headline({
     Key? key,
     this.text = 'Merry Christmas',
-    required this.textColor,
+    this.textColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: AppTextStyles.headline50(color: textColor),
+      style: AppTextStyles.headline50(
+        color: textColor ?? AppColors.blueColor,
+      ),
     );
   }
 }
